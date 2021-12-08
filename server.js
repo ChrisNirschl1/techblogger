fc908cc123aa9b95d1f980723153ad6cf2623336
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-08 01:41:11 -0800

    basic setup

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+// const session = require('express-session');
+// const exphbs = require('express-handlebars');
+// const allRoutes = require('./controllers');
+
+const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Requiring our models for syncing
+// const {User,Post,Opinion} = require('./models');
+
+
+// const sess = {
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {
+//         maxAge:1000*60*60*3
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
+
+// app.use(session(sess));
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // Static directory
+// app.use(express.static('public'));
+
+// const hbs = exphbs.create({});
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+ 
+// app.use('/',allRoutes);
+
+app.get("/", (req,res)=>{
+    res.send('hello')
+})
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('You have tuned in to the sweet sounds of PORT ' + PORT);
+    });
+});
