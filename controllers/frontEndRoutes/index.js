6283752c738ef6da71a4f07ef661809ac1700580
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-08 21:11:19 -0800

    cards

--- controllers/frontEndRoutes/index.js
+++ controllers/frontEndRoutes/index.js
@@ -6,4 +6,16 @@ router.get("/", (req, res)=> {
      res.render("home")
  })
 
+router.get("/posts", (req, res)=>{
+    Post.findAll().then(postData=>{
+        console.log(postData)
+        console.log("========")
+        const hbsPostData = postData.map(item=>item.get({plain:true}))
+        console.log(hbsPostData)
+        res.render("posts/index",{
+           post:hbsPostData 
+        })
+    })
+})
+
 module.exports = router;
